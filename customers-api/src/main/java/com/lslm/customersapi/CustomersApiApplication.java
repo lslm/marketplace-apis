f821ae57801b5665f506a64ccb174b481e64063d
Lucas S <lucas.santos.silva2011@gmail.com>
2022-06-16 18:36:51 -0300

    Enable Customers API service registration

--- customers-api/src/main/java/com/lslm/customersapi/CustomersApiApplication.java
+++ customers-api/src/main/java/com/lslm/customersapi/CustomersApiApplication.java
@@ -2,10 +2,12 @@ package com.lslm.customersapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableEurekaClient
 public class CustomersApiApplication {
 
 	public static void main(String[] args) {
