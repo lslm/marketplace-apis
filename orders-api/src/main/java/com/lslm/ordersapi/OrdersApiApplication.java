6e73f543da6ff48389924242f0141217f7bcc0c7
Lucas S <lucas.santos.silva2011@gmail.com>
2022-08-04 13:33:34 -0300

    Added migration to create Orders table

--- orders-api/src/main/java/com/lslm/ordersapi/OrdersApiApplication.java
+++ orders-api/src/main/java/com/lslm/ordersapi/OrdersApiApplication.java
@@ -3,9 +3,11 @@ package com.lslm.ordersapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableJpaAuditing
 public class OrdersApiApplication {
 
 	public static void main(String[] args) {
--- orders-api/src/main/java/com/lslm/ordersapi/entities/Order.java
+++ orders-api/src/main/java/com/lslm/ordersapi/entities/Order.java
@@ -1,42 +1,33 @@
 package com.lslm.ordersapi.entities;
 
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import javax.persistence.*;
+import java.time.Instant;
 import java.util.UUID;
 
+@Getter
+@Setter
+@Builder
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
-    @Column(nullable = false)
+    @Column(name = "product_id", nullable = false)
     private UUID productId;
 
     @Column(nullable = false)
     private int quantity;
 
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public UUID getProductId() {
-        return productId;
-    }
-
-    public void setProductId(UUID productId) {
-        this.productId = productId;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
 }
