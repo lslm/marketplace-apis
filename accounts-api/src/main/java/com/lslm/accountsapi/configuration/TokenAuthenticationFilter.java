171fb2886f38c4d582a7d1d7a3f11aae198e33f5
Lucas Santos <lucas.santos.silva2011@gmail.com>
2022-10-07 19:21:12 -0300

    Implement shouldNotFilter method

--- accounts-api/src/main/java/com/lslm/accountsapi/configuration/TokenAuthenticationFilter.java
+++ accounts-api/src/main/java/com/lslm/accountsapi/configuration/TokenAuthenticationFilter.java
@@ -24,29 +24,26 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
     @Autowired
     private AccountService accountService;
 
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        // Pegar o token do header da requisição
-        // Verificar se aquele token é válido
-        // Se o token for válido, devemos verificar se o usuário está cadastrado na API
-        String token = getTokenFromHeader(request);
-
-        String requestPath = request.getServletPath();
-
-        if (!requestPath.equals("/api/auth")) {
-            if (tokenService.isTokenValid(token)) {
-                authenticate(token);
-            }
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        if (request.getMethod().equals("POST")) {
+            return request.getServletPath().equals("/api/auth")
+                    || request.getServletPath().equals("/api/accounts");
         }
+        return false;
+    }
 
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = getTokenFromHeader(request);
+        if (tokenService.isTokenValid(token)) {
+            authenticate(token);
+        }
         filterChain.doFilter(request, response);
     }
 
     private void authenticate(String token) {
-        // obter o email do token
-        // e verificar se existe algum usuário para aquele email
-        // se existir um usuário, dizemos que a requisição é de um usuário altenticado
         String email = tokenService.getEmailFromToken(token);
-
         Optional<Account> maybeAccount = accountService.findByEmail(email);
         if (maybeAccount.isPresent()) {
             Account account = maybeAccount.get();
@@ -58,7 +55,6 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
 
     private String getTokenFromHeader(HttpServletRequest request) {
         String token = request.getHeader("Authorization");
-
         if(token == null || token.isEmpty())
             token = null;
 
